3f46e35f89b75e53b9c6d719c47ae0ec212142ae
Chris Ballinger <chrisballinger@gmail.com>
2011-08-18 00:39:38 -0400

    fix compile for iOS 4.x

--- Off the Record/main.m
+++ Off the Record/main.m
@@ -12,7 +12,8 @@
 
 int main(int argc, char *argv[])
 {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OTRAppDelegate class]));
-    }
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([OTRAppDelegate class]));
+    [pool release];
+    return retVal;
 }
