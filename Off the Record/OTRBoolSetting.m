dc36fea1c59bba67692118f8ebe6e43b2477d811
Chris Ballinger <chrisballinger@gmail.com>
2012-08-21 19:38:15 -0700

    Add File header

--- Off the Record/OTRBoolSetting.m
+++ Off the Record/OTRBoolSetting.m
@@ -5,6 +5,20 @@
 //  Created by Chris Ballinger on 4/11/12.
 //  Copyright (c) 2012 Chris Ballinger. All rights reserved.
 //
+//  This file is part of ChatSecure.
+//
+//  ChatSecure is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ChatSecure is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ChatSecure.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "OTRBoolSetting.h"
 
