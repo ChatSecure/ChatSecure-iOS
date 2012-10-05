0ceb76908288460381b35b7209bb4097b2b397bc
Chris Ballinger <chrisballinger@gmail.com>
2012-10-04 19:40:27 -0700

    Push account creation is working

--- Off the Record/OTRPushController.m
+++ Off the Record/OTRPushController.m
@@ -24,8 +24,12 @@
 #import "OTRPushAPIClient.h"
 #import "NSData+XMPP.h"
 
-#define SERVER_URL @"http://192.168.1.82:5000/"
+#define SERVER_URL @"http://192.168.1.102:5000/"
 #define REGISTER_PATH @"register"
+#define kOTRPushAccountKey @"kOTRPushAccountKey"
+#define ACCOUNT_ID_KEY @"account_id"
+#define PASSWORD_KEY @"password"
+#define EXPIRATION_DATE_KEY @"expiration_date"
 
 @implementation OTRPushController
 @synthesize pushClient;
@@ -66,13 +70,44 @@
     NSDictionary *parameters = [NSDictionary dictionaryWithObject:receiptString forKey:@"receipt-data"];
     [pushClient postPath:REGISTER_PATH parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSLog(@"response: %@", [responseObject description]);
+        NSString *accountID = [responseObject objectForKey:ACCOUNT_ID_KEY];
+        NSString *password = [responseObject objectForKey:PASSWORD_KEY];
+        NSString *expirationDateString = [responseObject objectForKey:EXPIRATION_DATE_KEY];
+        if (accountID && password && expirationDateString) {
+            NSMutableDictionary *accountDictionary = [self accountDictionary];
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+            dateFormatter.dateFormat = @"yyyy-MM-dd";
+            NSDate *expirationDate = [dateFormatter dateFromString:expirationDateString];
+            [accountDictionary setObject:accountID forKey:ACCOUNT_ID_KEY];
+            [accountDictionary setObject:password forKey:PASSWORD_KEY]; // TODO: store this in the keychain
+            [accountDictionary setObject:expirationDate forKey:EXPIRATION_DATE_KEY];
+            [self saveAccountDictionary:accountDictionary];
+        }
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error registering with receipt: %@%@", [error localizedDescription], [error userInfo]);
     }];
 }
 
+- (NSMutableDictionary*) accountDictionary {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *productDictionary = [NSMutableDictionary dictionaryWithDictionary:[defaults objectForKey:kOTRPushAccountKey]];
+    if (!productDictionary) {
+        productDictionary = [NSMutableDictionary dictionary];
+    }
+    return productDictionary;
+}
+
+- (void) saveAccountDictionary:(NSMutableDictionary*)productsDictionary {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:productsDictionary forKey:kOTRPushAccountKey];
+    BOOL success = [defaults synchronize];
+    if (!success) {
+        NSLog(@"Product preferences not saved to disk!");
+    }
+}
+
 - (void) updateDevicePushToken:(NSData *)devicePushToken {
-    NSLog(@"Updated device push token: %s", [devicePushToken bytes]);
+    NSLog(@"Updated device push token: %@", [devicePushToken hexStringValue]);
 }
 
 @end
