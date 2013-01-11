3b27841c260988697c2503cec5c4d07a5cdf7880
Chris Ballinger <chrisballinger@gmail.com>
2013-01-10 23:44:51 -0800

    Add NSManagedObject subclasses

---
+++ Off the Record/OTRManagedAccount.h
@@ -0,0 +1,55 @@
+//
+//  OTRManagedAccount.h
+//  Off the Record
+//
+//  Created by Christopher Ballinger on 1/10/13.
+//  Copyright (c) 2013 Chris Ballinger. All rights reserved.
+//
+//  This file is part of ChatSecure.
+//
+//  ChatSecure is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ChatSecure is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ChatSecure.  If not, see <http://www.gnu.org/licenses/>.
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#define kOTRAccountUsernameKey @"kOTRAccountUsernameKey"
+#define kOTRAccountProtocolKey @"kOTRAccountProtocolKey"
+#define kOTRAccountRememberPasswordKey @"kOTRAccountRememberPasswordKey"
+
+#define kAIMImageName @"aim.png"
+#define kGTalkImageName @"gtalk.png"
+#define kFacebookImageName @"facebook.png"
+#define kXMPPImageName @"xmpp.png"
+
+
+@interface OTRManagedAccount : NSManagedObject
+
+@property (nonatomic) BOOL isConnected;
+@property (nonatomic, retain) NSString * protocol;
+@property (nonatomic, readonly) BOOL rememberPassword;
+@property (nonatomic, retain) NSString *password; // nil if rememberPassword = NO, not stored in memory
+@property (nonatomic, retain) NSString * uniqueIdentifier;
+@property (nonatomic, retain, readonly) NSString * username;
+
+- (void) save;
+- (Class) protocolClass;
+- (NSString *) providerName;
+- (NSString *) imageName;
+
+- (void) setDefaultsWithProtocol:(NSString*)newProtocol;
+- (void) setNewUsername:(NSString *)newUsername;
+- (void) setShouldRememberPassword:(BOOL)remember;
+
+
+@end
---
+++ Off the Record/OTRManagedXMPPAccount.h
@@ -0,0 +1,39 @@
+//
+//  OTRManagedXMPPAccount.h
+//  Off the Record
+//
+//  Created by Christopher Ballinger on 1/10/13.
+//  Copyright (c) 2013 Chris Ballinger. All rights reserved.
+//
+//  This file is part of ChatSecure.
+//
+//  ChatSecure is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ChatSecure is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ChatSecure.  If not, see <http://www.gnu.org/licenses/>.
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "OTRManagedAccount.h"
+
+
+@interface OTRManagedXMPPAccount : OTRManagedAccount
+
+@property (nonatomic) BOOL allowSelfSignedSSL;
+@property (nonatomic) BOOL allowSSLHostNameMismatch;
+@property (nonatomic) BOOL sendDeliveryReceipts;
+@property (nonatomic) BOOL sendTypingNotifications;
+@property (nonatomic, retain) NSString * domain;
+@property (nonatomic) uint16_t port;
+
++(uint16_t)defaultPortNumber;
+
+@end
