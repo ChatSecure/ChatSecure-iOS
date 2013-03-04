8ba6aa9e64f2cb2bb82d15d850d2a204e38b7294
David Chiles <dwalterc@gmail.com>
2013-03-04 14:15:05 -0800

    made section headers with gradient

--- Off the Record/OTRSectionHeaderView.m
+++ Off the Record/OTRSectionHeaderView.m
@@ -7,6 +7,7 @@
 //
 
 #import "OTRSectionHeaderView.h"
+#import <QuartzCore/QuartzCore.h>
 
 @implementation OTRSectionHeaderView
 
@@ -42,8 +43,9 @@
         UILabel *label = [[UILabel alloc] initWithFrame:titleLabelFrame];
         label.text = title;
         label.font = [UIFont boldSystemFontOfSize:15.0];
-        label.textColor = [UIColor blackColor];
+        label.textColor = [UIColor whiteColor];
         label.backgroundColor = [UIColor clearColor];
+        [label setShadowOffset:CGSizeMake(0, -2)];
         [self addSubview:label];
         _titleLabel = label;
         
@@ -63,6 +65,24 @@
         [self addSubview:button];
         _disclosureButton = button;
         
+        
+        static NSMutableArray *colors = nil;
+        if (colors == nil) {
+            colors = [[NSMutableArray alloc] initWithCapacity:3];
+            UIColor *color = nil;
+            //color = [UIColor colorWithRed:0.82 green:0.84 blue:0.87 alpha:1.0];
+            color = [UIColor colorWithWhite:.7 alpha:1.0];
+            [colors addObject:(id)[color CGColor]];
+            //color = [UIColor colorWithRed:0.41 green:0.41 blue:0.59 alpha:1.0];
+            color = [UIColor colorWithWhite:.4 alpha:1.0];
+            [colors addObject:(id)[color CGColor]];
+            //color = [UIColor colorWithRed:0.41 green:0.41 blue:0.59 alpha:1.0];
+            color = [UIColor colorWithWhite:.2 alpha:1.0];
+            [colors addObject:(id)[color CGColor]];
+        }
+        [(CAGradientLayer *)self.layer setColors:colors];
+        [(CAGradientLayer *)self.layer setLocations:[NSArray arrayWithObjects:[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:0.4], [NSNumber numberWithFloat:1.0], nil]];
+        
     }
     return self;
 }
@@ -77,13 +97,9 @@
     
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
++ (Class)layerClass {
+    
+    return [CAGradientLayer class];
 }
-*/
 
 @end
