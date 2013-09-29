2a4c10bc719cc8b4a020c2ecbf29bf4be1e4e242
Chris Ballinger <chrisballinger@gmail.com>
2013-09-29 14:02:08 -0700

    Fix core data migration crash

--- Off the Record/OTRUtilities.h
+++ Off the Record/OTRUtilities.h
@@ -8,16 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
+#import "OTRConstants.h"
+
 /*
  *  System Versioning Preprocessor Macros
  */
-
-#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
-#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
-#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
-#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending
-
 @interface OTRUtilities : NSObject
 
 
