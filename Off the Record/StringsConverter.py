a376e050c9a9e0dbf8ab7a0a27bd5fe5502d443d
David Chiles <dwalterc@gmail.com>
2013-10-22 18:04:50 -0700

    cleanup strings converter

--- Off the Record/StringsConverter.py
+++ Off the Record/StringsConverter.py
@@ -68,7 +68,7 @@ def json_to_strings(dict):
 def genStrings(fileName):
 	call(['genstrings',fileName])
 	call(['mv','./Localizable.strings','en.lproj/Localizable.strings'])
-	call(['rm','-rf',fileName])
+	
 
 
 
@@ -84,35 +84,6 @@ if __name__ == "__main__":
 	json_to_strings(json.load(file))
 	file.close()
 
-	genStrings('tempStrings.h')
-
-
-
-	# newFile = open('tempStrings.h', 'w')
-
-	# lookupDictionary = {}
-
-	# totalEN = 0;
-	# totalLOC = 0;
-
-	# for line in file:
-	# 	if '#define EN_' in line:
-	# 		lineSplitArray = line.split(' ',2)
-	# 		if len(lineSplitArray) > 2:
-	# 			totalEN+=1
-	# 			lookupDictionary[lineSplitArray[1].strip()]=lineSplitArray[2]
-				
-	# 	if 'NSLocalizedString' in line:
-	# 		totalLOC+=1
-	# 		commaSplit = line.split(',',1)
-	# 		parSplit = commaSplit[0].split('(',1)
-	# 		key = parSplit[1].strip()
-	# 		newFile.write( '#define a'+str(totalLOC) +' NSLocalizedString('+ lookupDictionary[key].strip() + ' , '+commaSplit[1].strip()+'\n')
-	# 		del lookupDictionary[key]
-			
-		
-	# file.close()
-
-	# print lookupDictionary
-	# print totalEN
-	# print totalLOC
+	fileName = 'tempStrings.h'
+	genStrings(fileName)
+	call(['rm','-rf',fileName])
