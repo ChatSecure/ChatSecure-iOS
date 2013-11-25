4c076adab68f8ade69a4857346ff25ca3c2ef6d4
David Chiles <dwalterc@gmail.com>
2013-11-25 13:24:36 -0800

    working on tests

---
+++ Off the Record Tests/test_OTRManagedAccount.m
@@ -0,0 +1,35 @@
+//
+//  test_OTRManagedAccount.m
+//  Off the Record
+//
+//  Created by David Chiles on 11/21/13.
+//  Copyright (c) 2013 Chris Ballinger. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "OTRManagedAccount.h"
+
+@interface test_OTRManagedAccount : XCTestCase
+
+@end
+
+@implementation test_OTRManagedAccount
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)test_CreateAccount;
+{
+    
+}
+
+@end
