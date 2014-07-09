a0360cd6c9792676150e711bb028f2a30946dae5
David Chiles <dwalterc@gmail.com>
2014-07-08 18:43:26 -0700

    added demo target

---
+++ Off the Record/OTRChatDemo.m
@@ -0,0 +1,13 @@
+//
+//  OTRChatDemo.m
+//  Off the Record
+//
+//  Created by David Chiles on 7/8/14.
+//  Copyright (c) 2014 Chris Ballinger. All rights reserved.
+//
+
+#import "OTRChatDemo.h"
+
+@implementation OTRChatDemo
+
+@end
