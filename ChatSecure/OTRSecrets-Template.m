0a37883e3fdd0c724518f9d7f216893a6c5b0874
chenglin <l800891@me.com>
2014-09-04 23:31:55 -0400

    remove file change

---
+++ ChatSecure/OTRSecrets-Template.m
@@ -0,0 +1,11 @@
+//
+//  OTRSecrets.m
+//  Off the Record
+//
+//  Created by Chris Ballinger on 12/9/11.
+//  Copyright (c) 2012 Chris Ballinger. All rights reserved.
+//
+
+NSString *const kOTRGoogleAppSecret = @"";
+NSString *const kOTRHockeyLiveIdentifier = @"";
+NSString *const kOTRHockeyBetaIdentifier = @"";
