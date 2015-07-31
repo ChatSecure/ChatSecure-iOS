2c2dda870de1e6ba3f22f15c43acd5eac8826dd7
Chris Ballinger <chrisballinger@gmail.com>
2015-07-31 16:01:50 -0700

    Stronger passphrase generation

--- ChatSecure/Classes/Utilities/OTRPasswordGenerator.m
+++ ChatSecure/Classes/Utilities/OTRPasswordGenerator.m
@@ -12,23 +12,11 @@ NSUInteger const OTRDefaultPasswordLength = 100;
 
 @implementation OTRPasswordGenerator
 
-+ (NSString *)passwordWithLength:(NSUInteger)length
-{
-    NSString *alphaString = @"abcdefghijklmnopqrstuvwxyz";
-    NSString *alphaUpperString = [alphaString uppercaseString];
-    NSString *digitsString = @"0123456789";
-    NSString *symbolsString = @"~!@#$%^&*+=?/|:;{}[]()-_,.";
-    
-    NSString *allCharacters = [NSString stringWithFormat:@"%@%@%@%@",alphaString,alphaUpperString,digitsString,symbolsString];
-    NSUInteger allCharactersLength = [allCharacters length];
-    
-    NSMutableString *password = [NSMutableString new];
-    for (int i = 0; i < length; i++) {
-        NSUInteger index = arc4random() % allCharactersLength;
-        [password appendFormat:@"%C",[allCharacters characterAtIndex:index]];
-    }
-    
-    return password;
++ (NSString *)passwordWithLength:(NSUInteger)length {
+    NSMutableData* passphraseData = [NSMutableData dataWithLength:length];
+    SecRandomCopyBytes(kSecRandomDefault, length, [passphraseData mutableBytes]);
+    NSString *passphrase = [passphraseData base64EncodedStringWithOptions:0];
+    return passphrase;
 }
 
 @end
