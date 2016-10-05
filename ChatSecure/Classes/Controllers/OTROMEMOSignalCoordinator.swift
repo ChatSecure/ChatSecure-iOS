a5abb5774bbd8552bd20f1121a3bf4159a86e697
David Chiles <david@davidchiles.org>
2016-10-05 13:44:18 -0700

    Able to prepare sessions with buddies.
    Fetches all necessary bundles for the device list stored.

--- ChatSecure/Classes/Controllers/OTROMEMOSignalCoordinator.swift
+++ ChatSecure/Classes/Controllers/OTROMEMOSignalCoordinator.swift
@@ -21,13 +21,15 @@ import YapDatabase
     public let databaseConnection:YapDatabaseConnection
     public weak var omemoModule:OMEMOModule?
     public weak var omemoModuleQueue:dispatch_queue_t?
+    public var callbackQueue:dispatch_queue_t
+    private let workQueue:dispatch_queue_t
     private var myJID:XMPPJID? {
         get {
             return omemoModule?.xmppStream.myJID
         }
     }
     let preKeyCount:UInt = 100
-    private var outStandingDeviceBundleRequests:[String: () -> Void]
+    private var outstandingDeviceBundleRequests:[String: (Bool) -> Void]
     /**
      Create a OTROMEMOSignalCoordinator for an account. 
      
@@ -39,7 +41,9 @@ import YapDatabase
         self.omemoStorageManager = OTROMEMOStorageManager(accountKey: accountYapKey, accountCollection: OTRAccount.collection(), databaseConnection: databaseConnection)
         self.accountYapKey = accountYapKey
         self.databaseConnection = databaseConnection
-        self.outStandingDeviceBundleRequests = [:]
+        self.outstandingDeviceBundleRequests = [:]
+        self.callbackQueue = dispatch_queue_create("OTROMEMOSignalCoordinator-callback", DISPATCH_QUEUE_SERIAL)
+        self.workQueue = dispatch_queue_create("OTROMEMOSignalCoordinator-work", DISPATCH_QUEUE_SERIAL)
     }
     
     /**
@@ -53,6 +57,16 @@ import YapDatabase
         return jid.isEqualToJID(ourJID, options: XMPPJIDCompareBare)
     }
     
+    /** Always call on internal work queue*/
+    private func callAndRemoveOutstandingBundleBlock(elementId:String,success:Bool) {
+        
+        guard let outstandingBlock = self.outstandingDeviceBundleRequests[elementId] else {
+            return
+        }
+        outstandingBlock(success)
+        self.outstandingDeviceBundleRequests.removeValueForKey(elementId)
+    }
+    
     /**
      Check if a buddy supports OMEMO. This checks if we've seen devices for this buddy.
      
@@ -64,55 +78,79 @@ import YapDatabase
         return self.omemoStorageManager.getDevicesForParentYapKey(buddyYapKey, yapCollection: OTRBuddy.collection()).count > 0
     }
     
-    public func prepareSessionWithBuddy(buddyYapKey:String, completion:() -> Void) {
+    /** 
+     This must be called before sending every message. It ensures that for every device there is a session and if not the bundles are fetched.
+     
+     - parameter buddyYapKey: The yap key for the buddy to check
+     - parameter completion: The completion closure called on callbackQueue. If it successfully was able to fetch all the bundles or if it wasn't necessary. If there were no devices for this buddy it will also return flase
+     */
+    public func prepareSessionWithBuddy(buddyYapKey:String, completion:(Bool) -> Void) {
         var devices:[OTROMEMODevice]? = nil
         var username:String? = nil
+        
+        //Get all the devices ID's for this buddy as well as their username for use with signal and XMPPFramework.
         self.databaseConnection.readWithBlock { (transaction) in
             let dev = self.omemoStorageManager.getDevicesForParentYapKey(buddyYapKey, yapCollection: OTRBuddy.collection(),transaction: transaction)
             if (dev.count == 0) {
                 //No devices so we can't go any further.
+                dispatch_async(self.callbackQueue, { 
+                    completion(false)
+                })
                 return
             }
             devices = dev
             username = OTRBuddy.fetchObjectWithUniqueID(buddyYapKey, transaction: transaction)?.username
         }
         
-        guard let devs = devices, buddyUsername = username else {
+        guard let devs = devices, let buddyUsername = username else {
+            dispatch_async(self.callbackQueue, {
+                completion(false)
+            })
             return
         }
-        
-        
-        let group = dispatch_group_create()
-        for device in devs {
-            if !self.signalEncryptionManager.sessionRecordExistsForUsername(buddyUsername, deviceId: device.deviceId.intValue) {
-                //No session for this buddy and device combo. We need to fetch the bundle.
-                
-                let elementId = NSUUID().UUIDString
-                
-                self.outStandingDeviceBundleRequests[elementId] = {
+        var finalSuccess = true
+        dispatch_async(self.workQueue) { 
+            let group = dispatch_group_create()
+            //For each device Check if we have a session. If not then we need to fetch it from their XMPP server.
+            for device in devs {
+                if !self.signalEncryptionManager.sessionRecordExistsForUsername(buddyUsername, deviceId: device.deviceId.intValue) {
+                    //No session for this buddy and device combo. We need to fetch the bundle.
                     
+                    let elementId = NSUUID().UUIDString
+                    
+                    dispatch_group_enter(group)
+                    // Hold on to a closure so that when we get the call back from OMEMOModule we can call this closure.
+                    self.outstandingDeviceBundleRequests[elementId] = { success in
+                        if (!success) {
+                            finalSuccess = false
+                        }
+                        
+                        dispatch_group_leave(group)
+                    }
+                    //Fetch the bundle
+                    self.omemoModule?.fetchBundleForDeviceId(device.deviceId.unsignedIntValue, jid: XMPPJID.jidWithString(buddyUsername), elementId: elementId)
                 }
-                self.omemoModule?.fetchBundleForDeviceId(device.deviceId.unsignedIntValue, jid: XMPPJID.jidWithString(buddyUsername), elementId: elementId)
+            }
+            
+            dispatch_group_notify(group, self.callbackQueue) {
+                completion(finalSuccess)
             }
         }
-        
-        
-        
     }
 }
 
 extension OTROMEMOSignalCoordinator: OMEMOModuleDelegate {
     
     public func omemo(omemo: OMEMOModule, publishedDeviceIds deviceIds: [NSNumber], responseIq: XMPPIQ, outgoingIq: XMPPIQ) {
-        
+        //print("publishedDeviceIds: \(responseIq)")
     }
     
     public func omemo(omemo: OMEMOModule, failedToPublishDeviceIds deviceIds: [NSNumber], errorIq: XMPPIQ?, outgoingIq: XMPPIQ) {
-        
+        //print("failedToPublishDeviceIds: \(errorIq)")
     }
     
     public func omemo(omemo: OMEMOModule, deviceListUpdate deviceIds: [NSNumber], fromJID: XMPPJID, incomingElement: DDXMLElement) {
-        
+        //print("deviceListUpdate: \(fromJID) \(deviceIds)")
     }
     
     public func omemo(omemo: OMEMOModule, failedToFetchDeviceIdsForJID fromJID: XMPPJID, errorIq: XMPPIQ?, outgoingIq: XMPPIQ) {
@@ -120,17 +158,41 @@ extension OTROMEMOSignalCoordinator: OMEMOModuleDelegate {
     }
     
     public func omemo(omemo: OMEMOModule, publishedBundle bundle: OMEMOBundle, responseIq: XMPPIQ, outgoingIq: XMPPIQ) {
-        
+        //print("publishedBundle: \(responseIq) \(outgoingIq)")
     }
     
     public func omemo(omemo: OMEMOModule, failedToPublishBundle bundle: OMEMOBundle, errorIq: XMPPIQ?, outgoingIq: XMPPIQ) {
-        
+        //print("failedToPublishBundle: \(errorIq) \(outgoingIq)")
     }
     
     public func omemo(omemo: OMEMOModule, fetchedBundle bundle: OMEMOBundle, fromJID: XMPPJID, responseIq: XMPPIQ, outgoingIq: XMPPIQ) {
         
+        if (self.isOurJID(fromJID)) {
+            //We fetched our own bundle?
+            return;
+        }
+        
+        dispatch_async(self.workQueue) {
+            
+            //Create incoming bundle from OMEMOBundle
+            let innerBundle = OTROMEMOBundle(deviceId: bundle.deviceId, publicIdentityKey: bundle.identityKey, signedPublicPreKey: bundle.signedPreKey.publicKey, signedPreKeyId: bundle.signedPreKey.preKeyId, signedPreKeySignature: bundle.signedPreKey.signature)
+            //Select random pre key to use
+            let index = Int(arc4random_uniform(UInt32(bundle.preKeys.count)))
+            let preKey = bundle.preKeys[index]
+            let incomingBundle = OTROMEMOBundleIncoming(bundle: innerBundle, preKeyId: preKey.preKeyId, preKeyData: preKey.publicKey)
+            //Consume the incoming bundle. This goes through signal and should hit the storage delegate. So we don't need to store ourselves here.
+            self.signalEncryptionManager.consumeIncomingBundle(fromJID.bare(), bundle: incomingBundle)
+            let elementId = outgoingIq.elementID()
+            self.callAndRemoveOutstandingBundleBlock(elementId, success: true)
+        }
+        
     }
     public func omemo(omemo: OMEMOModule, failedToFetchBundleForDeviceId deviceId: gl_uint32_t, fromJID: XMPPJID, errorIq: XMPPIQ?, outgoingIq: XMPPIQ) {
+        
+        dispatch_async(self.workQueue) {
+            let elementId = outgoingIq.elementID()
+            self.callAndRemoveOutstandingBundleBlock(elementId, success: false)
+        }
         
     }
     
