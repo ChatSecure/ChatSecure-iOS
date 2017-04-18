6fd3e3bc9a299dc793d85d6296bfdde7deecb49c
N-Pex <npexdev@gmail.com>
2017-04-18 10:41:54 +0200

    Add migration view

---
+++ ChatSecure/Classes/Views/MigrationInfoHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  MigrationInfoHeaderView.swift
+//  ChatSecure
+//
+//  Created by N-Pex on 2017-04-17.
+//  Copyright © 2017 Chris Ballinger. All rights reserved.
+//
+
+import UIKit
+
+public class MigrationInfoHeaderView: UIView {
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var descriptionLabel: UILabel!
+    @IBOutlet public var startButton: UIButton!
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel.preferredMaxLayoutWidth = titleLabel.bounds.width
+        descriptionLabel.preferredMaxLayoutWidth = descriptionLabel.bounds.width
+    }
+}
