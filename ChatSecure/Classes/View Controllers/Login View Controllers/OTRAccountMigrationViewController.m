016f4393b5f15ff033bcfbff13d7113afbcf9ee1
Chris Ballinger <chrisballinger@gmail.com>
2017-05-09 15:29:42 -0700

    Move most migration code to OTRAccountMigrator helper class. Now attempts to login disconnected accounts before starting migration.

--- ChatSecure/Classes/View Controllers/Login View Controllers/OTRAccountMigrationViewController.m
+++ ChatSecure/Classes/View Controllers/Login View Controllers/OTRAccountMigrationViewController.m
@@ -15,6 +15,7 @@
 #import "OTRYapMessageSendAction.h"
 #import "OTRLog.h"
 #import "OTRXMPPManager_Private.h"
+#import "OTRAccountMigrator.h"
 
 NSString *const kSpamYourContactsTag = @"kSpamYourContactsTag";
 
@@ -29,6 +30,7 @@ typedef NS_ENUM(NSInteger, MigrationStatus) {
 @interface OTRAccountMigrationViewController ()
 /** Whether or not the account is migrated within handleSuccessWithNewAccount:. This is to maybe fix a bug where the contacts are re-added multiple times. */
 @property (nonatomic) MigrationStatus migrationStatus;
+@property (nonatomic, strong, nullable) OTRAccountMigrator *migrator;
 @end
 
 @implementation OTRAccountMigrationViewController
@@ -86,102 +88,18 @@ typedef NS_ENUM(NSInteger, MigrationStatus) {
         return;
     }
     self.migrationStatus = MigrationStatusMigrating;
-    [OTRDatabaseManager.shared.readWriteDatabaseConnection readWriteWithBlock:^(YapDatabaseReadWriteTransaction *transaction) {
-        [newAccount saveWithTransaction:transaction];
-    }];
-    // This is where we do the migration before passing off to the superclass
-    
-    OTRXMPPManager *oldXmpp = (OTRXMPPManager*)[[OTRProtocolManager sharedInstance] protocolForAccount:self.oldAccount];
-    OTRXMPPManager *newXmpp = (OTRXMPPManager*)[[OTRProtocolManager sharedInstance] protocolForAccount:newAccount];
-    
-    NSParameterAssert(oldXmpp);
-    NSParameterAssert(newXmpp);
-    
     BOOL shouldSpamFriends = [[self.form formRowWithTag:kSpamYourContactsTag].value isEqual:@YES];
-    
-    // Step 1 - Add old contacts to new account
-    
-    NSString *messageText = [NSString stringWithFormat:@"%@: %@", MY_NEW_ACCOUNT_INFO_STRING(), newAccount.bareJID.bare];
-    NSMutableArray<OTROutgoingMessage*> *outgoingMessages = [NSMutableArray array];
-    __block NSArray<OTRXMPPBuddy*> *buddies = @[];
-    __block NSMutableArray<OTRBuddy*> *newBuddies = [NSMutableArray array];
-    [[OTRDatabaseManager sharedInstance].readOnlyDatabaseConnection readWithBlock:^(YapDatabaseReadTransaction * _Nonnull transaction) {
-        buddies = [self.oldAccount allBuddiesWithTransaction:transaction];
-        newBuddies = [NSMutableArray arrayWithCapacity:buddies.count];
-        [buddies enumerateObjectsUsingBlock:^(OTRXMPPBuddy * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            // Don't add yourself to your new roster
-            if ([obj.bareJID isEqualToJID:self.oldAccount.bareJID options:XMPPJIDCompareBare]) {
-                return;
-            }
-            OTRXMPPBuddy *newBuddy = [[OTRXMPPBuddy alloc] init];
-            newBuddy.username = obj.username;
-            newBuddy.accountUniqueId = newAccount.uniqueId;
-            // Show buddies in list only if you've talked to them before
-            if (obj.lastMessageId.length > 0 && !obj.isArchived) {
-                newBuddy.lastMessageId = @"";
-            }
-            newBuddy.isArchived = obj.isArchived;
-            newBuddy.avatarData = obj.avatarData;
-            newBuddy.displayName = obj.displayName;
-            newBuddy.preferredSecurity = obj.preferredSecurity;
-            [newBuddies addObject:newBuddy];
-            
-            // If spamming friends, create some messages for them
-            if (shouldSpamFriends) {
-                OTROutgoingMessage *message = [OTROutgoingMessage messageToBuddy:obj text:messageText transaction:transaction];
-                [outgoingMessages addObject:message];
-            }
-        }];
+    self.migrator = [[OTRAccountMigrator alloc] initWithOldAccount:self.oldAccount migratedAccount:newAccount shouldSpamFriends:shouldSpamFriends];
+    __weak typeof(self)weakSelf = self;
+    [self.migrator migrateWithCompletion:^(BOOL success, NSError * _Nullable error) {
+        __strong typeof(weakSelf)strongSelf = weakSelf;
+        if (success) {
+            strongSelf.migrationStatus = MigrationStatusComplete;
+        } else {
+            strongSelf.migrationStatus = MigrationStatusFailed;
+        }
+        [super handleSuccessWithNewAccount:newAccount sender:sender];
     }];
-    
-    
-    [OTRDatabaseManager.shared.readWriteDatabaseConnection readWriteWithBlock:^(YapDatabaseReadWriteTransaction * _Nonnull transaction) {
-        [newBuddies enumerateObjectsUsingBlock:^(OTRBuddy * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            [obj saveWithTransaction:transaction];
-        }];
-    }];
-    [newXmpp addBuddies:newBuddies];
-    
-    // Step 2 - Message old contacts that you have new account
-    
-    if (shouldSpamFriends) {
-        [oldXmpp enqueueMessages:outgoingMessages];
-    }
-    
-    // Step 3 - Copy your avatar from old account to new account
-    
-    [newXmpp setAvatar:self.oldAccount.avatarImage completion:^(BOOL success) {
-        DDLogVerbose(@"Avatar copied to new account: success=%d", success);
-    }];
-    
-    // Step 4 - Update your old account's vCard with new JID
-    
-    XMPPvCardTemp *vCard = self.oldAccount.vCardTemp;
-    vCard.jid = newAccount.bareJID;
-    self.oldAccount.waitingForvCardTempFetch = NO;
-    self.oldAccount.lastUpdatedvCardTemp = [NSDate date];
-    [OTRDatabaseManager.shared.readWriteDatabaseConnection readWriteWithBlock:^(YapDatabaseReadWriteTransaction * _Nonnull transaction) {
-        [self.oldAccount saveWithTransaction:transaction];
-    }];
-    [oldXmpp.xmppvCardTempModule updateMyvCardTemp:vCard];
-    
-    // Step 5 - Update your old account's vCard.image to force other client's to refresh your whole vCard
-    
-    [oldXmpp setAvatar:self.oldAccount.avatarImage completion:^(BOOL success) {
-        DDLogVerbose(@"Avatar copied to on account to force vCard update: success=%d", success);
-    }];
-    
-    // Step 6 - Mark your old conversations as 'archived'
-    
-    [OTRDatabaseManager.shared.readWriteDatabaseConnection readWriteWithBlock:^(YapDatabaseReadWriteTransaction * _Nonnull transaction) {
-        [buddies enumerateObjectsUsingBlock:^(OTRXMPPBuddy * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            obj = [obj copy];
-            obj.isArchived = YES;
-            [obj saveWithTransaction:transaction];
-        }];
-    }];
-    self.migrationStatus = MigrationStatusComplete;
-    [super handleSuccessWithNewAccount:newAccount sender:sender];
 }
 
 
