70a4ad8ef27919fad7fbcf24220ab2d60e59dedd
N-Pex <npexdev@gmail.com>
2017-08-04 09:43:30 +0200

    Add support for custom header and footer cells

--- ChatSecure/Classes/View Controllers/OTRRoomOccupantsViewController.swift
+++ ChatSecure/Classes/View Controllers/OTRRoomOccupantsViewController.swift
@@ -12,14 +12,22 @@ import PureLayout
 
 public class OTRRoomOccupantsViewController: UIViewController {
     
-    let tableView: UITableView
-    let viewHandler: OTRYapViewHandler
-    let roomKey: String
+    @IBOutlet open weak var tableView:UITableView!
+    open var viewHandler:OTRYapViewHandler?
+    open var roomKey:String?
+    open var room:OTRXMPPRoom?
+    open var headerRows:[String] = []
+    open var footerRows:[String] = []
     static let CellIdentifier = "Cell"
     
     public init(databaseConnection:YapDatabaseConnection, roomKey:String) {
         self.tableView = UITableView(frame: .zero, style: .plain)
         self.roomKey = roomKey
+        databaseConnection.read({ (transaction) in
+            if let key = self.roomKey {
+                self.room = OTRXMPPRoom.fetchObject(withUniqueID: key, transaction: transaction)
+            }
+        })
         viewHandler = OTRYapViewHandler(databaseConnection: databaseConnection)
         super.init(nibName: nil, bundle: nil)
         setupViewHandler()
@@ -39,14 +47,30 @@ public class OTRRoomOccupantsViewController: UIViewController {
         self.tableView.autoPinEdgesToSuperviewEdges()
         self.tableView.dataSource = self
         self.tableView.delegate = self
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: OTRRoomOccupantsViewController.CellIdentifier)
-    }
-
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        setupTableView()
+        self.tableView.register(OTRBuddyInfoCell.self, forCellReuseIdentifier: OTRRoomOccupantsViewController.CellIdentifier)
     }
     
+    open func createHeaderCell(indexPath:IndexPath, type:String) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    open func createFooterCell(indexPath:IndexPath, type:String) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    open func didSelectHeaderCell(indexPath:IndexPath, type:String) {
+    }
+    
+    open func didSelectFooterCell(indexPath:IndexPath, type:String) {
+    }
+    
+    open func heightForHeaderCell(indexPath:IndexPath, type:String) -> CGFloat {
+        return 44
+    }
+    
+    open func heightForFooterCell(indexPath:IndexPath, type:String) -> CGFloat {
+        return 44
+    }
 }
 
 extension OTRRoomOccupantsViewController: OTRYapViewHandlerDelegateProtocol {
@@ -64,35 +88,86 @@ extension OTRRoomOccupantsViewController: OTRYapViewHandlerDelegateProtocol {
 extension OTRRoomOccupantsViewController: UITableViewDataSource {
     //Int and UInt issue https://github.com/yapstudios/YapDatabase/issues/116
     public func numberOfSections(in tableView: UITableView) -> Int {
-        if let sections = self.viewHandler.mappings?.numberOfSections() {
-            return Int(sections)
+        if let sections = self.viewHandler?.mappings?.numberOfSections() {
+            return 2 + Int(sections)
         }
-        return 0
+        return 2
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let rows = self.viewHandler.mappings?.numberOfItems(inSection: UInt(section)) {
+        if isHeaderSection(section: section) {
+            return headerRows.count
+        } else if isFooterSection(section: section) {
+            return footerRows.count
+        }
+        if let rows = self.viewHandler?.mappings?.numberOfItems(inSection: UInt(section - 1)) {
             return Int(rows)
         }
         return 0
     }
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: OTRRoomOccupantsViewController.CellIdentifier, for: indexPath)
-        
-        if let roomOccupant = self.viewHandler.object(indexPath) as? OTRXMPPRoomOccupant {
-            cell.textLabel?.text = roomOccupant.realJID ?? roomOccupant.jid
-        } else {
-            cell.detailTextLabel?.text = ""
-            cell.textLabel?.text = ""
+    open func isHeaderSection(section:Int) -> Bool {
+        return section == 0
+    }
+    
+    open func isFooterSection(section:Int) -> Bool {
+        if let sections = self.viewHandler?.mappings?.numberOfSections() {
+            if (section == (Int(sections) + 1)) {
+                return true
+            }
+            return false
         }
-        
+        return section == 1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if isHeaderSection(section: indexPath.section) {
+            return createHeaderCell(indexPath: indexPath, type: headerRows[indexPath.row])
+        } else if isFooterSection(section: indexPath.section) {
+            return createFooterCell(indexPath: indexPath, type: footerRows[indexPath.row])
+        }
+        let adjustedIndexPath = IndexPath(row: indexPath.row, section: indexPath.section - 1)
+        let cell:OTRBuddyInfoCell = tableView.dequeueReusableCell(withIdentifier: OTRRoomOccupantsViewController.CellIdentifier, for: indexPath) as! OTRBuddyInfoCell
+        var buddy:OTRXMPPBuddy? = nil
+        if let roomOccupant = self.viewHandler?.object(adjustedIndexPath) as? OTRXMPPRoomOccupant, let room = self.room {
+            OTRDatabaseManager.shared.readOnlyDatabaseConnection?.read({ (transaction) in
+                    buddy = OTRXMPPBuddy.fetch(withUsername: roomOccupant.realJID ?? roomOccupant.jid!, withAccountUniqueId: room.accountUniqueId!, transaction: transaction)
+            })
+            if let buddy = buddy {
+                cell.setThread(buddy, account: nil)
+            }
+        }
         cell.selectionStyle = .none
-        
         return cell
     }
 }
 
 extension OTRRoomOccupantsViewController:UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        if isHeaderSection(section: indexPath.section) {
+            return heightForHeaderCell(indexPath:indexPath, type:headerRows[indexPath.row])
+        } else if isFooterSection(section: indexPath.section) {
+            return heightForFooterCell(indexPath:indexPath, type:footerRows[indexPath.row])
+        }
+        return 80.0
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if isHeaderSection(section: indexPath.section) {
+            return heightForHeaderCell(indexPath:indexPath, type:headerRows[indexPath.row])
+        } else if isFooterSection(section: indexPath.section) {
+            return heightForFooterCell(indexPath:indexPath, type:footerRows[indexPath.row])
+        }
+        return 80.0
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if isHeaderSection(section: indexPath.section) {
+            didSelectHeaderCell(indexPath:indexPath, type:headerRows[indexPath.row])
+        } else if isFooterSection(section: indexPath.section) {
+            didSelectFooterCell(indexPath:indexPath, type:footerRows[indexPath.row])
+        }
+    }
     
 }
