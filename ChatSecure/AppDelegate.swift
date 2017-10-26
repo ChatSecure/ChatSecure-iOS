d1e9a363dba88daa48a812adae1308a64a11d16c
Chris Ballinger <chrisballinger@gmail.com>
2017-10-25 23:03:40 -0700

    Use Swift AppDelegate entry point

---
+++ ChatSecure/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  ChatSecure
+//
+//  Created by Chris Ballinger on 10/25/17.
+//  Copyright © 2017 Chris Ballinger. All rights reserved.
+//
+
+import Foundation
+import ChatSecureCore
+
+// Use Swift entry point for better debugging in Xcode 9.1+
+@UIApplicationMain
+class AppDelegate: OTRAppDelegate {}
