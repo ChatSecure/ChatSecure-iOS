863a5e6d7737d2f62ffb4d20797e361051c359cb
Chris Ballinger <chrisballinger@gmail.com>
2019-11-02 16:49:21 -0700

    Fix info plist

--- ChatSecure/InfoPlist.h
+++ ChatSecure/InfoPlist.h
@@ -6,10 +6,8 @@
 //  Copyright © 2019 Chris Ballinger. All rights reserved.
 //
 
-#pragma once
-
-#if (!defined(TARGET_OS_MACCATALYST) || (TARGET_OS_MACCATALYST==0))
-#define EXPORT_COMPLIANCE_CODE 51d17d3e-5e07-49ad-a308-9625d81e411f
-#else
+#if TARGET_OS_MACCATALYST
 // TODO: Get Mac App Store export compliance code
+#else
+#define EXPORT_COMPLIANCE_CODE 51d17d3e-5e07-49ad-a308-9625d81e411f
 #endif
