8de2fc9fa7f056cd743d7d2a69297a1749c32e5d
Chris Ballinger <chrisballinger@gmail.com>
2019-11-03 11:18:46 -0800

    Fixing catalyst resource bundle issues

--- ChatSecure/InfoPlist.h
+++ ChatSecure/InfoPlist.h
@@ -8,6 +8,7 @@
 
 #if TARGET_OS_MACCATALYST
 // TODO: Get Mac App Store export compliance code
+#define EXPORT_COMPLIANCE_CODE xxxxxx
 #else
 #define EXPORT_COMPLIANCE_CODE 51d17d3e-5e07-49ad-a308-9625d81e411f
 #endif
